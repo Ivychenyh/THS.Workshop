fc14877a5b587fd9960673e78d1aa5c04a172b89
Ivy Chen <fay.chenya@gmail.com>
2021-07-03 14:22:35 +0800

    測試方案掛上OWIN

--- THS.Workshop.InfrastructureTest/SpecflowHook.cs
+++ THS.Workshop.InfrastructureTest/SpecflowHook.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using System.Data.Entity.SqlServer;
 using System.Diagnostics.Contracts;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Web.Http;
 using TechTalk.SpecFlow;
 using THS.Workshop.Infrastructure.DataAccess.EntityModel;
+using Microsoft.Owin.Hosting;
+using Owin;
+using THS.Workshop.WebApi;
 
 namespace THS.Workshop.InfrastructureTest
 {
+    
     [Binding]
     public class SpecflowHook
     {
-        public static readonly string   TestData     = "出發吧，跟我一起進入偉大的航道";
-        public static readonly DateTime TestDateTime = new DateTime(1900, 1, 1, 0, 0, 0);
-        public static readonly string   TestUserId   = "TEST_USER";
+        public static readonly string      TestData     = "出發吧，跟我一起進入偉大的航道";
+        public static readonly DateTime    TestDateTime = new DateTime(1900, 1, 1, 0, 0, 0);
+        public static readonly string      TestUserId   = "TEST_USER";
+        private const          string      HOST_ADDRESS = "https://localhost:44316/";
+        private static         IDisposable s_webApp;
+        public static          HttpClient  s_client;
 
+        
         [AfterScenario]
         public void AfterScenario()
         {
@@ -25,6 +35,7 @@ namespace THS.Workshop.InfrastructureTest
         [AfterTestRun]
         public static void AfterTestRun()
         {
+            s_webApp.Dispose();
             using (var dbContext = MemberDbContext.Create())
             {
                 if (dbContext.Database.Exists())
@@ -54,7 +65,29 @@ namespace THS.Workshop.InfrastructureTest
 
                 dbContext.Database.Initialize(true);
             }
+            s_webApp = WebApp.Start<Startup>(HOST_ADDRESS);
+            Console.WriteLine("Web API started!");
+            s_client             = new HttpClient();
+            s_client.BaseAddress = new Uri(HOST_ADDRESS);
+            Console.WriteLine("HttpClient started!");
+           
         }
+
+        public class Startup
+        {
+            public void Configuration(IAppBuilder app)
+            {
+                var configuration = new HttpConfiguration();
+                WebApiConfig.Register(configuration);
+#if DEBUG
+                app.UseErrorPage();
+#endif
+                app.UseWelcomePage("/Welcome");
+                app.UseWebApi(configuration);
+
+                //        }
+            }
+        } 
 
         public string DeleteAll()
         {
--- THS.Workshop.InfrastructureTest/會員管理API測試Steps.cs
+++ THS.Workshop.InfrastructureTest/會員管理API測試Steps.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -9,17 +8,17 @@ using THS.Workshop.Infrastructure.DomainModel.Member;
 
 namespace THS.Workshop.InfrastructureTest
 {
+
     [Binding]
     public class 會員管理API測試Steps : Steps
     {
         [When(@"調用Post新增會員資料")]
         public void When調用Post新增會員資料()
         {
-            var request = this.ScenarioContext.Get<InsertRequest>("request");
-            var client  = new HttpClient {BaseAddress = new Uri("https://localhost:44316/")};
-            var json = JsonConvert.SerializeObject(request) ;
-            var content = new StringContent( json, Encoding.UTF8, "application/json");
-            var response = client.PostAsync("api/Member/Post", content).Result;
+            var request  = this.ScenarioContext.Get<InsertRequest>("request");
+            var json     = JsonConvert.SerializeObject(request) ;
+            var content  = new StringContent( json, Encoding.UTF8, "application/json");
+            var response = SpecflowHook.s_client.PostAsync("api/Member/Post", content).Result;
             ScenarioContext.Set(response.StatusCode, "actual");
         }
 
@@ -29,5 +28,7 @@ namespace THS.Workshop.InfrastructureTest
             var actual = this.ScenarioContext.Get<HttpStatusCode>("actual");
             Assert.AreEqual(p0, actual);
         }
+
     }
+   
 }
